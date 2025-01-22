a5200ac5c7d425df914bf9c5480f29f77b47f350
AndreyVolkovDev <154226271+AndreyVolkovDev@users.noreply.github.com>
2025-01-22 18:28:01 +0300

    Create practical6.py

---
+++ practical6.py
@@ -0,0 +1,8 @@
+# Волков А.А Зит-23 090302_237315
+def to_lowercase(text):
+    return text.lower()
+
+# Пример использования
+input_text = "Привет, Мир!"
+output_text = to_lowercase(input_text)
+print(output_text)
